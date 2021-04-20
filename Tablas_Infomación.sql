fb56b3baf720f89d6d103e910530e612027484cd
ivanfm-JulioVerne <81305805+ivanfm-JulioVerne@users.noreply.github.com>
2021-04-20 11:11:06 +0200

    Update Tablas_Infomación.sql

--- Tablas_Infomación.sql
+++ Tablas_Infomación.sql
@@ -1,18 +1,25 @@
+--
+DROP TABLE empleado;
+DROP TABLE cliente;
+
 CREATE TABLE empleado
 (
     cod_empleado NUMBER CONSTRAINT coe_emp_pk PRIMARY KEY,
-    nombre CHAR CONSTRAINT nom_emp_nn NOT NULL CONSTRAINT nom_emp_un UNIQUE,
+    nombre VARCHAR2(60) CONSTRAINT nom_emp_nn NOT NULL CONSTRAINT nom_emp_un UNIQUE,
     jefe NUMBER,
     telefono NUMBER(9),
     ultima_jornada CHAR,
     
-    CONSTRAINT emp_fk FOREIGN KEY (jefe) REFERENCES empleado
+    CONSTRAINT coj_emp_fk FOREIGN KEY (jefe) REFERENCES empleado
 );
-
+DROP TABLE empleado;
 CREATE TABLE cliente
 (
-    cod_cliente NUMBER,
-    nombre
+    cod_cliente NUMBER CONSTRAINT coc_cli_pk PRIMARY KEY,
+    nombre VARCHAR(60) CONSTRAINT nom_cli_nn NOT NULL CONSTRAINT nom_cli_un UNIQUE,
+    vip CHAR(2) CONSTRAINT vip_cli_ck CHECK (vip IN ('SI' || 'NO')) CONSTRAINT vip_cli_nn NOT NULL,
+    telefono NUMBER(9)
 );
+DELETE empleado;
 
-INSERT INTO empleado (cod_empleado, nombre, jefe, telefono, ultima_jornada) VALUE (1,José,null,123456789,null);
+INSERT INTO cliente VALUES (1,'José','SI',123456789);
